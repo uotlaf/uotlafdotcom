eccc7a7e9e90eb33d1a6d12977268a10fd9e0213
uotlaf <git@uotlaf.com>
2024-07-20 14:10:03 -0300

    Initial support for language/i18n

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
                 ArtistSeeder::class,
-                ArtsSeeder::class
+                ArtsSeeder::class,
             ]
         );
     }
